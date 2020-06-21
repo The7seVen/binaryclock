0f98c02d9f4f007223db35859d87a6094f95e1bf
Adam Bieszk <adambieszk6@gmail.com>
2020-06-21 15:30:24 +0200

    Update website

--- js/hourToBinaryClock.js
+++ js/hourToBinaryClock.js
@@ -5,9 +5,9 @@ function hourToBinaryClock(){
        result.innerHTML = "<span style='color:red'>Proszę wpisać godzinę do konwersji!</span>";
    }
    else {
-       let regHour = "^(0-2)[1](0-9)[1]\:(0-5)[1](0-9)[1]\:(0-5)[1](0-9)[1]$";
+       let regHour = /^((?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$)/;
        if(!regHour.test(hourToConvert)){
-        result.innerHTML = "<span style='color:red'> Proszę wpisać poprawną formę godziny </span>";
+        result.innerHTML = "<span style='color:red'> Proszę wpisać poprawną formę godziny! </span>";
        }
        else {
         result.innerHTML = "<span style='color:green'>Poprawna godzina!</span>";
